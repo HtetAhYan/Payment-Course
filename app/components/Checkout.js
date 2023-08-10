2cdfbf774399163415afa3169a7cb0c892716dcf
HtetAhYan <htetahyan@gmail.com>
2023-08-11 02:20:37 +0630

    please

--- app/components/Checkout.js
+++ app/components/Checkout.js
@@ -1,9 +1,10 @@
 "use client"
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from "@mui/material";
 import { useRouter } from 'next/navigation';
 function Checkout({ total,items }) {
   const router = useRouter()
+   const [buttonClicked, setButtonClicked] = useState(false);
   const handled = async () => {
     
     const data = await fetch('/api/payment/Stripe', {
@@ -17,6 +18,12 @@ function Checkout({ total,items }) {
     const { url } = await data.json()
    router.push(url)
   }
+  useEffect(() => {
+    if (buttonClicked) {
+      handled();
+    }
+  }, [buttonClicked]);
+
   return (
        <><div className="mt-4">
         <h3 className="text-lg font-semibold mb-2">Total:</h3>
@@ -25,7 +32,7 @@ function Checkout({ total,items }) {
       <Button
         variant="contained"
         color="primary"
-        onClick={handled}
+        onClick={()=> setButtonClicked(true) }
         className="mt-4"
       >
         Checkout
