c8c6d02c47c3518c930ee80a8436173ea36b4349
HtetAhYan <htetahyan@gmail.com>
2023-08-11 01:49:17 +0630

    testssss

---
+++ app/api/payment/Stripe/route.js
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import Stripe from 'stripe'
+export const stripe = new Stripe(process.env.STRIPE_KEY || "", {
+  apiVersion: "2022-11-15",
+  typescript: false
+})
+export const POST = async(request) => {
+  const items = await request.json();
+
+ const itemss = [{slug:'dfdfd',price:299}]
+  try {
+    const CheckoutSession = await stripe.checkout.sessions.create({
+      line_items: items.map(item => ({ 
+        
+        price_data: {
+          currency: "usd",
+          product_data: {
+            name: item.webinarData.slug,
+
+          },unit_amount: item.webinarData.price *100
+        },quantity: 1
+      })),
+      mode: 'payment',
+      success_url: "http://localhost:3000/success",
+     cancel_url:"http://localhost:3000/cancel"
+    })
+return NextResponse.json({url : CheckoutSession.url})
+  } catch (error) {
+  
+    return NextResponse.json({error:error.message,items},{status:500})
+  }
+}
